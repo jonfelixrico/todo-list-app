ef9fd32ea34219524cb634bcf0db7be820dd7fce
Felix <jonfelixrico@gmail.com>
2022-02-25 21:04:49 +0800

    Adjust deprecated v-deep usage

--- src/components/dialogs/CTaskCreateDialog.vue
+++ src/components/dialogs/CTaskCreateDialog.vue
@@ -111,7 +111,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.notes-input ::v-deep .q-field__control-container {
+.notes-input ::v-deep(.q-field__control-container) {
   min-height: 300px;
 }
 </style>
