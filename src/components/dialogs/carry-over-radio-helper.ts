e4074d0ec05ce5632aa9f0f86b943a84fb3abd9e
Felix <jonfelixrico@gmail.com>
2022-02-26 03:03:44 +0800

    Create helper for the carry over radio

---
+++ src/components/dialogs/carry-over-radio-helper.ts
@@ -0,0 +1,64 @@
+import { date } from 'quasar'
+import { CarryOver } from 'src/typings/task.interface'
+import { Ref, computed, ref } from 'vue'
+
+export type TransformedCarryOver = 'NO_CARRY_OVER' | 'INDEFINITE' | 'DEFINITE'
+
+export function useCarryOverRadioHelper(
+  carryOver: Ref<CarryOver>,
+  targetDt: Ref<Date>
+) {
+  const definiteDate = ref(date.addToDate(targetDt.value, { day: 1 }))
+
+  const dateModel = computed({
+    get: () => {
+      const clamped = Math.max(
+        definiteDate.value.getTime(),
+        targetDt.value.getTime()
+      )
+
+      return new Date(clamped)
+    },
+
+    set: (newDate: Date) => {
+      carryOver.value = definiteDate.value = newDate = newDate
+    },
+  })
+
+  const radioModel = computed({
+    get: () => {
+      if (!carryOver.value) {
+        return 'NO_CARRY_OVER'
+      } else if (carryOver.value === 'INDEFINITE') {
+        return 'INDEFINITE'
+      } else {
+        // carryOver instanceof Date is true
+        return 'DEFINITE'
+      }
+    },
+
+    set: (val: TransformedCarryOver) => {
+      switch (val) {
+        case 'NO_CARRY_OVER': {
+          carryOver.value = null
+          break
+        }
+
+        case 'DEFINITE': {
+          carryOver.value = dateModel.value
+          break
+        }
+
+        case 'INDEFINITE': {
+          carryOver.value = 'INDEFINITE'
+          break
+        }
+      }
+    },
+  })
+
+  return {
+    dateModel,
+    radioModel,
+  }
+}
