32ed73439bb3d6ca8ec2831ec5fbec91db72ba5d
Felix <jonfelixrico@gmail.com>
2022-02-25 20:44:07 +0800

    Create date pagination system in Tasks

--- src/pages/Tasks.vue
+++ src/pages/Tasks.vue
@@ -12,47 +12,80 @@
         @click="onWriteBtnClick"
       />
     </q-toolbar>
+    <q-toolbar class="bg-secondary"> </q-toolbar>
     <div class="col">list goes here</div>
   </q-page>
 </template>
 
 <script lang="ts">
 import { date, useQuasar } from 'quasar'
-import { defineComponent } from 'vue'
+import { computed, defineComponent } from 'vue'
 import { useStore } from 'src/store'
 import { Task } from 'src/typings/task.interface'
 import { CreateTaskInput } from 'src/store/tasks/actions'
 import CTaskCreateDialog, {
   TaskDraft,
 } from 'src/components/dialogs/CTaskCreateDialog.vue'
+import { useRouter } from 'vue-router'
+
+function useTaskCreate() {
+  const $q = useQuasar()
+
+  const store = useStore()
+  const createTask = (toCreate: CreateTaskInput) =>
+    store.dispatch('tasks/createTask', toCreate) as Promise<Task>
+
+  const doCreate = async (task: TaskDraft, targetDt: Date) => {
+    const { id } = await createTask({ targetDt, ...task } as CreateTaskInput)
+    console.debug('Created task %s.', id)
+  }
+
+  const onWriteBtnClick = () => {
+    const targetDt = date.startOfDate(new Date(), 'day')
+    $q.dialog({
+      component: CTaskCreateDialog,
+      componentProps: {
+        persistent: true,
+      },
+    }).onOk((task: TaskDraft) => {
+      void doCreate(task, targetDt)
+    })
+  }
+
+  return { onWriteBtnClick }
+}
+
+function useDateNavigation() {
+  const router = useRouter()
+
+  const routeDate = computed(() => {
+    const dateStr = router.currentRoute.value.params.date as string
+    if (!date.isValid(dateStr)) {
+      return null
+    }
+
+    return new Date(dateStr)
+  })
+
+  async function setRouteDate(toDate: Date) {
+    const formatted = date.formatDate(toDate, 'DD-MM-YYYY')
+    await router.push({
+      name: 'tasksFiltered',
+      params: { date: formatted },
+    })
+  }
+
+  return {
+    routeDate,
+    setRouteDate,
+  }
+}
 
 export default defineComponent({
   setup() {
-    const $q = useQuasar()
-
-    const store = useStore()
-    const createTask = (toCreate: CreateTaskInput) =>
-      store.dispatch('tasks/createTask', toCreate) as Promise<Task>
-
-    const doCreate = async (task: TaskDraft, targetDt: Date) => {
-      const { id } = await createTask({ targetDt, ...task } as CreateTaskInput)
-      console.debug('Created task %s.', id)
-    }
-
-    const onWriteBtnClick = () => {
-      const targetDt = date.startOfDate(new Date(), 'day')
-      $q.dialog({
-        component: CTaskCreateDialog,
-        componentProps: {
-          persistent: true,
-        },
-      }).onOk((task: TaskDraft) => {
-        void doCreate(task, targetDt)
-      })
-    }
-
     return {
-      onWriteBtnClick,
+      ...useTaskCreate(),
+      ...useDateNavigation(),
     }
   },
 })
