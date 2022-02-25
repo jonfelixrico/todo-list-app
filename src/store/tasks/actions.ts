94f13ea2f8ec5d79284a74bda11fa49c4e924225
Felix <jonfelixrico@gmail.com>
2022-02-25 19:31:50 +0800

    Create 'createTask' action

--- src/store/tasks/actions.ts
+++ src/store/tasks/actions.ts
@@ -1,10 +1,24 @@
+import { date, uid } from 'quasar'
+import { Task, TaskDraft } from 'src/typings/task.interface'
 import { ActionTree } from 'vuex'
 import { StateInterface } from '../index'
 import { ITasksState } from './state'
 
 const actions: ActionTree<ITasksState, StateInterface> = {
-  someAction(/* context */) {
-    // your code
+  createTask({ commit }, draft: TaskDraft) {
+    const createDt = new Date()
+
+    const task: Task = {
+      ...draft,
+      createDt,
+      lastUpdateDt: createDt,
+      targetDt: date.startOfDate(createDt, 'day'),
+      id: uid(),
+    }
+
+    commit('addTask', task)
+
+    return task
   },
 }
 
