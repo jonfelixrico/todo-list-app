73badd0662e09c501b43417c5c6732bdd87f94ea
Felix <jonfelixrico@gmail.com>
2022-02-25 19:25:40 +0800

    Implement the use of the mutation in Tasks

--- src/pages/Tasks.vue
+++ src/pages/Tasks.vue
@@ -17,26 +17,35 @@
 </template>
 
 <script lang="ts">
-import { useQuasar } from 'quasar'
+import { uid, useQuasar } from 'quasar'
 import { defineComponent } from 'vue'
-import CWriteTaskDialogVue from 'src/components/dialogs/CTaskCreateDialog.vue'
+import CWriteTaskDialogVue, {
+  TaskDraft,
+} from 'src/components/dialogs/CTaskCreateDialog.vue'
+import { useStore } from 'src/store'
+import { Task } from 'src/typings/task.interface'
 
 export default defineComponent({
   setup() {
     const $q = useQuasar()
+    const store = useStore()
     const onWriteBtnClick = () => {
       $q.dialog({
         component: CWriteTaskDialogVue,
         componentProps: {
           persistent: true,
         },
+      }).onOk((task: TaskDraft) => {
+        const createDt = new Date()
+        const id = uid()
+        store.commit('tasks/addTask', {
+          ...task,
+          createDt,
+          lastUpdateDt: createDt,
+          id,
+        } as Task)
+        console.log('Task %s created.', id)
       })
-        .onOk(() => {
-          console.debug('ogey')
-        })
-        .onCancel(() => {
-          console.debug('rrat')
-        })
     }
 
     return {
