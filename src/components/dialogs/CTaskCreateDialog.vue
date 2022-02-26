81a667a422abce3fc0693da4f882ee9c8179400c
Felix <jonfelixrico@gmail.com>
2022-02-26 17:08:04 +0800

    Integrate i18n into CTaskCreateDialog

--- src/components/dialogs/CTaskCreateDialog.vue
+++ src/components/dialogs/CTaskCreateDialog.vue
@@ -2,87 +2,108 @@
   <q-dialog ref="dialogRef" @hide="onDialogHide" :persistent="persistent">
     <q-card class="q-dialog-plugin">
       <q-card-section>
-        <h6 class="text-h6 q-my-none">Add To-do</h6>
-        <div class="text-caption">For {{ formattedTargetDt }}</div>
+        <h6 class="text-h6 q-my-none">{{ t('dialogs.taskCreate.title') }}</h6>
+        <div class="text-caption">
+          {{ t('dialogs.taskCreate.targetDtLbl') }}
+        </div>
       </q-card-section>
       <q-separator />
       <q-card-section class="q-gutter-y-md">
+        <!-- Title -->
         <q-input
           v-model="task.title"
           type="textarea"
           outlined
           autogrow
           dense
-          label="Title"
-          hint="Description or title of what you want to do."
+          :label="t('dialogs.taskCreate.inputs.title.label')"
+          :hint="t('dialogs.taskCreate.inputs.title.hint')"
         />
+
+        <!-- Notes -->
         <q-input
           v-model="task.notes"
           type="textarea"
           outlined
           autogrow
           dense
-          label="Notes"
           class="notes-input"
-          hint="Optional. Place additional details of your task here."
+          :label="t('dialogs.taskCreate.inputs.notes.label')"
+          :hint="t('dialogs.taskCreate.inputs.notes.hint')"
         />
+
+        <!-- Priority -->
         <q-input
           v-model.number="task.priority"
           type="number"
           outlined
-          label="Priority"
           min="1"
           max="10"
           step="0.1"
           dense
-          hint="The priority of what you want to do, raging from 1 to 10."
+          :label="t('dialogs.taskCreate.inputs.priority.label')"
+          :hint="t('dialogs.taskCreate.inputs.priority.hint')"
         />
+
+        <!-- Carry-over -->
         <div class="column">
+          <!-- No carry over -->
           <q-radio
             v-model="carryOver.radioModel"
             val="NO_CARRY_OVER"
-            label="No carry over"
+            :label="t('dialogs.taskCreate.inputs.carryOver.options.no')"
           />
+
+          <!-- Carry over until completed -->
           <q-radio
             v-model="carryOver.radioModel"
             val="INDEFINITE"
-            label="Carry over until completed"
+            :label="
+              t('dialogs.taskCreate.inputs.carryOver.options.untilCompleted')
+            "
           />
+
+          <!-- Carry over until date (if not yet completed) -->
           <q-radio v-model="carryOver.radioModel" val="DEFINITE">
-            <div class="row items-center">
-              Carry over until
-              <q-input
-                outlined
-                dense
-                v-model="carryOver.dateModel"
-                :disable="carryOver.radioModel !== 'DEFINITE'"
-                :bottom-slots="false"
-              >
-                <template v-slot:append>
-                  <q-icon name="event" class="cursor-pointer">
-                    <q-popup-proxy
-                      ref="qDateProxy"
-                      cover
-                      transition-show="scale"
-                      transition-hide="scale"
-                      mask="date"
-                      :rules="['date']"
-                    >
-                      <q-date v-model="carryOver.dateModel">
-                        <div class="row items-center justify-end">
-                          <q-btn
-                            v-close-popup
-                            label="Close"
-                            color="primary"
-                            flat
-                          />
-                        </div>
-                      </q-date>
-                    </q-popup-proxy>
-                  </q-icon>
-                </template>
-              </q-input>
-            </div>
+            <i18n-t
+              keypath="dialogs.taskCreate.inputs.carryOver.options.untilDate"
+              tag="div"
+              class="row items-center preformatted"
+            >
+              <template #date>
+                <q-input
+                  outlined
+                  dense
+                  v-model="carryOver.dateModel"
+                  :disable="carryOver.radioModel !== 'DEFINITE'"
+                  :bottom-slots="false"
+                >
+                  <template v-slot:append>
+                    <q-icon name="event" class="cursor-pointer">
+                      <q-popup-proxy
+                        ref="qDateProxy"
+                        cover
+                        transition-show="scale"
+                        transition-hide="scale"
+                        mask="date"
+                        :rules="['date']"
+                      >
+                        <q-date v-model="carryOver.dateModel">
+                          <div class="row items-center justify-end">
+                            <q-btn
+                              v-close-popup
+                              label="Close"
+                              color="primary"
+                              flat
+                            />
+                          </div>
+                        </q-date>
+                      </q-popup-proxy>
+                    </q-icon>
+                  </template>
+                </q-input>
+              </template>
+            </i18n-t>
           </q-radio>
         </div>
       </q-card-section>
@@ -92,13 +113,14 @@
           color="primary"
           noCaps
           unelevated
-          :label="t('common.ok')"
+          :label="t('dialogs.taskCreate.buttons.create')"
           @click="onDialogOk"
         />
+
         <q-btn
           noCaps
-          flat
-          :label="t('common.cancel')"
+          outline
+          :label="t('dialogs.taskCreate.buttons.discard')"
           @click="onDialogCancel"
         />
       </q-card-actions>
