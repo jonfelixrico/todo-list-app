8478f365e4ff4475d09526d68decc5086ad7c54d
Felix <jonfelixrico@gmail.com>
2022-02-27 00:57:33 +0800

    Add more methods to TaskRepoService

--- src/services/abstracts/task-repo.service.ts
+++ src/services/abstracts/task-repo.service.ts
@@ -1,8 +1,10 @@
 import { Task } from 'src/typings/task.interface'
-import { inject, InjectionKey } from 'vue'
+import { inject, InjectionKey, Ref } from 'vue'
 
 export interface TaskRepoService {
   getTasks: (snapshotDate: Date) => Promise<Task[]>
+  lastWrite: Ref<number>
+  getDaysWithTasks: () => Promise<Date[]>
 }
 
 export const TaskRepoServiceKey: InjectionKey<TaskRepoService> =
