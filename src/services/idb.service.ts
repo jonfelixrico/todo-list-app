34bda5df3e08c067a56a29ea49c0f4a378c7b444
Felix <jonfelixrico@gmail.com>
2022-02-27 01:06:48 +0800

    Make IDBService use service-boot.type

--- src/services/idb.service.ts
+++ src/services/idb.service.ts
@@ -1,5 +1,6 @@
 import { IDBPDatabase, openDB } from 'idb'
 import { App, inject, InjectionKey } from 'vue'
+import { ServiceBootFn } from './service-boot.type'
 
 const DB_NAME = 'todo.idb'
 // TODO update this per schema change
@@ -7,12 +8,13 @@ const DB_VERSION = 1
 
 export const IdbServiceKey: InjectionKey<IDBPDatabase> = Symbol('idb')
 
-export default async ({ app }: { app: App }) => {
+export function useIdb() {
+  return inject(IdbServiceKey)
+}
+
+const boot: ServiceBootFn = async ({ app }: { app: App }) => {
   const db = await openDB(DB_NAME, DB_VERSION)
   app.provide(IdbServiceKey, db)
   console.log('IndexedDB-promised: started and provided.')
 }
-
-export function useIdb() {
-  return inject(IdbServiceKey)
-}
+export default boot
