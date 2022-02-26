3ec2629ce4711e9c09e3547ebbd4b7b09030646e
Felix <jonfelixrico@gmail.com>
2022-02-27 02:12:59 +0800

    Apply fixes to the way the IDB is initialized

--- src/idb/index.ts
+++ src/idb/index.ts
@@ -5,14 +5,18 @@ const DB_NAME = 'todo.idb'
 // TODO update this per schema change
 const DB_VERSION = 1
 
-type Schema = TasksIdbStore
+export type IdbSchema = TasksIdbStore
 import tasksUpgradeCb from './tasks.idb-store'
 
 export async function start() {
-  return await openDB<Schema>(DB_NAME, DB_VERSION, {
+  const db = await openDB<IdbSchema>(DB_NAME, DB_VERSION, {
     upgrade(...args) {
       tasksUpgradeCb(...args)
       console.debug('IndexedDB-promised: DB ugpraded.')
     },
   })
+
+  console.log('IndexedDB-promised: started.')
+
+  return db
 }
--- src/services/idb.service.ts
+++ src/services/idb.service.ts
@@ -1,20 +1,18 @@
-import { IDBPDatabase, openDB } from 'idb'
+import { IDBPDatabase } from 'idb'
+import { IdbSchema, start } from 'src/idb'
 import { App, inject, InjectionKey } from 'vue'
 import { ServiceBootFn } from './service-boot.type'
 
-const DB_NAME = 'todo.idb'
-// TODO update this per schema change
-const DB_VERSION = 1
-
-export const IdbServiceKey: InjectionKey<IDBPDatabase> = Symbol('idb')
+export const IdbServiceKey: InjectionKey<IDBPDatabase<IdbSchema>> =
+  Symbol('idb')
 
 export function useIdb() {
   return inject(IdbServiceKey)
 }
 
 const boot: ServiceBootFn = async ({ app }: { app: App }) => {
-  const db = await openDB(DB_NAME, DB_VERSION)
+  const db = await start()
   app.provide(IdbServiceKey, db)
-  console.log('IndexedDB-promised: started and provided.')
+  console.debug('IdbService: provided.')
 }
 export default boot
