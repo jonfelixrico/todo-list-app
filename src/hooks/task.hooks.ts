db149a4eb1e2bae3dc14c634aabf72aa0f29634e
Felix <jonfelixrico@gmail.com>
2022-02-27 02:04:58 +0800

    Add logging to useTasksFetcher

--- src/hooks/task.hooks.ts
+++ src/hooks/task.hooks.ts
@@ -2,14 +2,16 @@ import { useTaskRepo } from 'src/services/abstracts/task-repo.service'
 import { Task } from 'src/typings/task.interface'
 import { ref, Ref, watch } from 'vue'
 
-export function useFilteredTaskRetrieve(date: Ref<Date>) {
+export function useTasksFetcher(date: Ref<Date>) {
   const { getTasks, lastWrite } = useTaskRepo()
   const data = ref<Task[]>([])
 
   watch(
     [lastWrite, date],
     async () => {
-      data.value = await getTasks(date.value)
+      console.debug('useTasksFetcher: getting tasks for %s.', date.value)
+      const res = (data.value = await getTasks(date.value))
+      console.debug('useTasksFetcher: retrieved %d records.', res.length)
     },
     {
       immediate: true,
