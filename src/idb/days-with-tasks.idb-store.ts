3ccadc99d29cd82abbbf7d4d10f72b3c279aaed0
Felix <jonfelixrico@gmail.com>
2022-02-27 16:09:57 +0800

    Define key in DaysWithTasks

--- src/idb/days-with-tasks.idb-store.ts
+++ src/idb/days-with-tasks.idb-store.ts
@@ -1,15 +1,22 @@
 import { DBSchema } from 'idb'
 import type { IdbUgpradeCb } from 'src/idb/idb.schema'
 
+export interface DaysWithTasksEntry {
+  date: Date
+  count: number
+}
+
 export interface DaysWithTasksIdbStore extends DBSchema {
   daysWithTasks: {
-    key: number
-    value: number
+    key: Date
+    value: DaysWithTasksEntry
   }
 }
 
 const upgradeCb: IdbUgpradeCb = (db) => {
-  db.createObjectStore('daysWithTasks')
+  db.createObjectStore('daysWithTasks', {
+    keyPath: 'date',
+  })
   console.debug('IndexedDB-promised: daysWithTasks store upgraded.')
 }
 
