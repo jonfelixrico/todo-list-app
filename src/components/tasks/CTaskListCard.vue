2c02c7ec06fdaa885c2d94a0a51a9e3511ae8960
Felix <jonfelixrico@gmail.com>
2022-02-28 23:28:19 +0800

    Add edit and delete events in CTaskListCard

--- src/components/tasks/CTaskListCard.vue
+++ src/components/tasks/CTaskListCard.vue
@@ -1,6 +1,6 @@
 <template>
   <q-card>
-    <q-card-section class="row">
+    <q-card-section class="row q-gutter-x-md">
       <div class="col q-gutter-y-md">
         <div>
           <h5 class="text-h6 q-ma-none preformatted" v-text="title" />
@@ -15,6 +15,25 @@
           class="preformatted q-pa-sm bg-grey-3 rounded-borders"
         />
       </div>
+      <q-separator vertical />
+      <div class="column q-gutter-y-sm">
+        <q-btn
+          :label="t('common.edit')"
+          flat
+          color="primary"
+          no-caps
+          dense
+          @click="onEdit"
+        />
+        <q-btn
+          :label="t('common.delete')"
+          flat
+          color="negative"
+          no-caps
+          dense
+          @click="onDelete"
+        />
+      </div>
     </q-card-section>
   </q-card>
 </template>
@@ -22,6 +41,7 @@
 <script lang="ts">
 import { Task } from 'src/typings/task.interface'
 import { defineComponent, PropType, toRefs } from 'vue'
+import { useI18n } from 'vue-i18n'
 
 export default defineComponent({
   props: {
@@ -31,11 +51,16 @@ export default defineComponent({
     },
   },
 
-  setup(props) {
+  emits: ['edit', 'delete'],
+
+  setup(props, { emit }) {
     const task = toRefs(props.task)
 
     return {
       ...task,
+      t: useI18n().t,
+      onEdit: () => emit('edit', task),
+      onDelete: () => emit('delete', task),
     }
   },
 })
