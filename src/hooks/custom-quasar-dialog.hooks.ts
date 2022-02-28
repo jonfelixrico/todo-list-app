66e4d9825045b7db22c746666d5bd86f7accf0be
Felix <jonfelixrico@gmail.com>
2022-03-01 00:01:55 +0800

    Create custom-quasar-dialog-hooks

---
+++ src/hooks/custom-quasar-dialog.hooks.ts
@@ -0,0 +1,43 @@
+import { QDialogOptions, useQuasar } from 'quasar'
+import { useI18n } from 'vue-i18n'
+
+export function useCustomQuasarDialog() {
+  const { dialog } = useQuasar()
+  const { t } = useI18n()
+
+  const OK_BTN = {
+    unelevated: true,
+    color: 'primary',
+    noCaps: true,
+    label: t('common.ok'),
+  }
+
+  const CANCEL_BTN = {
+    flat: true,
+    color: 'standard',
+    noCaps: true,
+    label: t('common.cancel'),
+  }
+
+  function create(options: QDialogOptions) {
+    return dialog({
+      ...options,
+      ok:
+        typeof options?.ok === 'object'
+          ? { ...OK_BTN, ...options.ok }
+          : options.ok,
+      cancel:
+        typeof options?.cancel === 'object'
+          ? { ...CANCEL_BTN, ...options.cancel }
+          : options.cancel,
+    })
+  }
+
+  return {
+    /**
+     * Wrapper for $q.dialog. We've made the default button props to be
+     * standard across the app.
+     */
+    create,
+  }
+}
