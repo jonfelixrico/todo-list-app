b154ed51a5f80d61ba3985f0e1a5df0e96b25ef8
Felix <jonfelixrico@gmail.com>
2022-03-01 00:15:42 +0800

    Adjust custom-quasar hooks

--- src/hooks/custom-quasar.hooks.ts
+++ src/hooks/custom-quasar.hooks.ts
@@ -5,6 +5,9 @@ export function useCustomQuasarDialog() {
   const { dialog } = useQuasar()
   const { t } = useI18n()
 
+  /**
+   * Default settings for OK
+   */
   const OK_BTN = {
     unelevated: true,
     color: 'primary',
@@ -12,6 +15,9 @@ export function useCustomQuasarDialog() {
     label: t('common.ok'),
   }
 
+  /**
+   * Default settings for Cancel
+   */
   const CANCEL_BTN = {
     flat: true,
     color: 'standard',
@@ -19,17 +25,44 @@ export function useCustomQuasarDialog() {
     label: t('common.cancel'),
   }
 
+  /**
+   * These wrap methods will "blend-in" the value provided by the user into the button defaults.
+   */
+
+  const wrapOk = (prop: QDialogOptions['ok']): QDialogOptions['ok'] => {
+    if (prop === false) {
+      return prop
+    } else if (prop === true || prop === undefined) {
+      return OK_BTN
+    } else if (typeof prop === 'object') {
+      return { ...OK_BTN, ...prop }
+    } else {
+      // fallback
+      return undefined
+    }
+  }
+
+  const wrapCancel = (
+    prop: QDialogOptions['cancel']
+  ): QDialogOptions['cancel'] => {
+    if (prop === false || prop === undefined) {
+      // cancel is not shown by default; needs object or `true` for it to show up
+      return prop
+    } else if (prop === true) {
+      return CANCEL_BTN
+    } else if (typeof prop === 'object') {
+      return { ...CANCEL_BTN, ...prop }
+    } else {
+      // fallback
+      return undefined
+    }
+  }
+
   function createDialog(options: QDialogOptions) {
     return dialog({
       ...options,
-      ok:
-        typeof options?.ok === 'object'
-          ? { ...OK_BTN, ...options.ok }
-          : options.ok,
-      cancel:
-        typeof options?.cancel === 'object'
-          ? { ...CANCEL_BTN, ...options.cancel }
-          : options.cancel,
+      ok: wrapOk(options.ok),
+      cancel: wrapCancel(options.cancel),
     })
   }
 
