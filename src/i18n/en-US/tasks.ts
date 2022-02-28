7c81d89f51c58923eb05e4509c28e92486c2de84
Felix <jonfelixrico@gmail.com>
2022-02-28 23:35:30 +0800

    Create tasks i18n

---
+++ src/i18n/en-US/tasks.ts
@@ -0,0 +1,10 @@
+export default {
+  dialogs: {
+    deleteTaskConfirm: {
+      confirm: 'Yes, delete',
+      cancel: 'Cancel',
+      title: 'Delete Task',
+      message: 'Are you sure you want to delete this task?',
+    },
+  },
+}
