9ef671a6934ed94f187863bba7aa053b53ac5092
Felix <jonfelixrico@gmail.com>
2022-03-03 23:48:44 +0800

    Create max method

--- src/utils/date.utils.ts
+++ src/utils/date.utils.ts
@@ -1,13 +1,33 @@
+const START_OF_EPOCH = new Date(0)
+
 /**
-import { date } from 'quasar';
  * Returns the smallest date in terms of their time millis.
  *
  * @param dates
  * @returns The smallest date. If empty `dates` arugment was provided, then the
  * start of the epoch will be returned instead (1970/1/1).
  */
-export function getMinDate(...dates: Date[]) {
+export function getMinDate(...dates: Date[]): Date {
+  if (!dates.length) {
+    return START_OF_EPOCH
+  }
+
   const converted = dates.map((date) => +date)
-  const minVal = Math.min(...converted)
-  return new Date(minVal === Infinity ? 0 : minVal)
+  return new Date(Math.min(...converted))
+}
+
+/**
+ * Returns the largest date in terms of their time millis.
+ *
+ * @param dates
+ * @returns The largest date. If empty `dates` arugment was provided, then the
+ * start of the epoch will be returned instead (1970/1/1).
+ */
+export function getMaxDate(...dates: Date[]): Date {
+  if (!dates.length) {
+    return START_OF_EPOCH
+  }
+
+  const converted = dates.map((date) => +date)
+  return new Date(Math.max(...converted))
 }
--- test/jest/__tests__/utils/date.utils.spec.ts
+++ test/jest/__tests__/utils/date.utils.spec.ts
@@ -1,16 +1,28 @@
-import { getMinDate } from 'src/utils/date.utils'
+import { getMaxDate, getMinDate } from 'src/utils/date.utils'
 import { describe, expect, it } from '@jest/globals'
 
-describe('getMinDate', () => {
-  const expectedMinDate = new Date('1998/06/21')
-  const referenceDate = new Date('2022/1/1')
+const min = new Date('1998/06/21')
+const mid = new Date('2022/1/1')
+const max = new Date('2077/1/1')
 
-  it('can get the minimum date given an array of dates', () => {
-    const res = getMinDate(referenceDate, new Date(), expectedMinDate)
-    expect(res).toEqual(expectedMinDate)
+describe('getMinDate', () => {
+  it('can get the earliest date given an array of dates', () => {
+    const res = getMinDate(min, mid, max)
+    expect(res).toEqual(min)
   })
 
   it('will give the start of epoch date if used empty', () => {
     expect(getMinDate()).toEqual(new Date(0))
+  })
+})
+
+describe('getMaxDate', () => {
+  it('can get the latest date given an array of dates', () => {
+    const res = getMaxDate(min, mid, max)
+    expect(res).toEqual(max)
+  })
+
+  it('will give the start of epoch date if used empty', () => {
+    expect(getMaxDate()).toEqual(new Date(0))
   })
 })
