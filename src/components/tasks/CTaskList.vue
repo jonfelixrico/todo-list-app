ec86f11f92594fb7bf4e1656bae958384d86d2b5
Felix <jonfelixrico@gmail.com>
2022-03-04 03:49:36 +0800

    Use CTaskListItem in CTaskList

--- src/components/tasks/CTaskList.vue
+++ src/components/tasks/CTaskList.vue
@@ -1,47 +1,6 @@
 <template>
   <q-list separator>
-    <q-item
-      clickable
-      v-for="{ title, id, notes, priority } of tasks"
-      :key="id"
-      class="q-pa-md"
-    >
-      <div class="row full-width q-gutter-x-sm">
-        <div class="q-ml-none">
-          <q-checkbox :modelValue="true" />
-        </div>
-
-        <q-separator vertical />
-
-        <div class="col column justify-center q-gutter-y-sm">
-          <div class="row">
-            <div class="col">
-              <!--
-                Since we'll be using q-gutter-* in the parent, we want to wrap to avoid the
-                margin CSS from conflicting
-              -->
-              <h6 class="preformatted text-h6 q-my-none" v-text="title" />
-            </div>
-
-            <div class="row items-center q-gutter-x-sm">
-              <q-badge>MOCK_CARRIED_OVER</q-badge>
-              <q-badge v-if="priority">Priority {{ priority }}</q-badge>
-            </div>
-          </div>
-          <div
-            class="preformatted q-pa-sm bg-grey-3 rounded-borders"
-            v-if="notes"
-            v-text="notes"
-          />
-        </div>
-
-        <q-separator vertical />
-
-        <div>
-          <q-btn color="primary" no-caps dense flat round icon="info" />
-        </div>
-      </div>
-    </q-item>
+    <CTaskListItem v-for="task of tasks" :key="task.id" :task="task" />
   </q-list>
 </template>
 
@@ -51,6 +10,7 @@ import { uid } from 'quasar'
 import { computed, defineComponent, Ref, ref, toRefs, watch } from 'vue'
 import { Task } from 'src/typings/task.interface'
 import { useI18n } from 'vue-i18n'
+import CTaskListItem from 'src/components/tasks/CTaskListItem.vue'
 
 function useTasksFetcher(date: Ref<Date>) {
   const { getTasks, lastWrite } = useTaskRepo()
@@ -102,11 +62,12 @@ export default defineComponent({
     const { t } = useI18n()
     const { snapshotDt } = toRefs(props)
     const { tasks } = useTasksFetcher(snapshotDt)
-
     return {
       tasks,
       t,
     }
   },
+
+  components: { CTaskListItem },
 })
 </script>
