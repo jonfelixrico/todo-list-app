28f487abfdf40f04aec91e448328fd153f9a20ec
Felix <jonfelixrico@gmail.com>
2022-03-04 03:34:59 +0800

    Adjust UI

--- src/components/tasks/CTaskList.vue
+++ src/components/tasks/CTaskList.vue
@@ -2,18 +2,45 @@
   <q-list separator>
     <q-item
       clickable
-      v-for="{ title, id, notes } of tasks"
+      v-for="{ title, id, notes, priority } of tasks"
       :key="id"
       class="q-pa-md"
     >
-      <q-item-section>
-        <h6 class="preformatted text-h6 q-my-none q-mb-sm" v-text="title" />
-        <div
-          class="preformatted q-pa-sm bg-grey-3 rounded-borders"
-          v-if="notes"
-          v-text="notes"
-        />
-      </q-item-section>
+      <div class="row full-width q-gutter-x-sm">
+        <div class="q-ml-none">
+          <q-checkbox :modelValue="true" />
+        </div>
+
+        <q-separator vertical />
+
+        <div class="col column justify-center q-gutter-y-sm">
+          <div class="row">
+            <div class="col">
+              <!--
+                Since we'll be using q-gutter-* in the parent, we want to wrap to avoid the
+                margin CSS from conflicting
+              -->
+              <h6 class="preformatted text-h6 q-my-none" v-text="title" />
+            </div>
+
+            <div class="row items-center q-gutter-x-sm">
+              <q-badge>MOCK_CARRIED_OVER</q-badge>
+              <q-badge v-if="priority">Priority {{ priority }}</q-badge>
+            </div>
+          </div>
+          <div
+            class="preformatted q-pa-sm bg-grey-3 rounded-borders"
+            v-if="notes"
+            v-text="notes"
+          />
+        </div>
+
+        <q-separator vertical />
+
+        <div>
+          <q-btn color="primary" no-caps dense flat round icon="info" />
+        </div>
+      </div>
     </q-item>
   </q-list>
 </template>
@@ -23,6 +50,7 @@ import { useTaskRepo } from 'src/services/abstracts/task-repo.service'
 import { uid } from 'quasar'
 import { computed, defineComponent, Ref, ref, toRefs, watch } from 'vue'
 import { Task } from 'src/typings/task.interface'
+import { useI18n } from 'vue-i18n'
 
 function useTasksFetcher(date: Ref<Date>) {
   const { getTasks, lastWrite } = useTaskRepo()
@@ -71,11 +99,13 @@ export default defineComponent({
   },
 
   setup(props) {
+    const { t } = useI18n()
     const { snapshotDt } = toRefs(props)
     const { tasks } = useTasksFetcher(snapshotDt)
 
     return {
       tasks,
+      t,
     }
   },
 })
