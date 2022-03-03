d4dcd625dba5d4f53c3523ad6da1ccd18da005ac
Felix <jonfelixrico@gmail.com>
2022-03-03 23:22:26 +0800

    Create date utils

---
+++ src/utils/date.utils.ts
@@ -0,0 +1,12 @@
+/**
+ * Returns the smallest date in terms of their time millis.
+ *
+ * @param dates
+ * @returns The smallest date. If empty `dates` arugment was provided, then the
+ * start of the epoch will be returned instead (1970/1/1).
+ */
+export function getMinDate(...dates: Date[]) {
+  const converted = dates.filter((date) => !!date).map((date) => +date)
+  const minVal = Math.min(...converted)
+  return new Date(minVal === Infinity ? 0 : minVal)
+}
