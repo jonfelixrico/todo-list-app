daef8f3ff6d8f84e59804956e331c94c7a5e2d3a
Felix <jonfelixrico@gmail.com>
2022-03-03 23:57:47 +0800

    Create getDaysBetween

--- src/utils/date.utils.ts
+++ src/utils/date.utils.ts
@@ -1,3 +1,5 @@
+import { date } from 'quasar'
+
 const START_OF_EPOCH = new Date(0)
 
 /**
@@ -30,4 +32,28 @@ export function getMaxDate(...dates: Date[]): Date {
 
   const converted = dates.map((date) => +date)
   return new Date(Math.max(...converted))
+}
+
+/**
+ * Gets the days between a start and end date. Inclusive.
+ * @param start
+ * @param end
+ * @returns The first item is the start of day of `start`. The last item is the
+ * start of day of `end`. The items in between are the start of day of the individual
+ * days in between.
+ */
+export function getDaysBetween(start: Date, end: Date): Date[] {
+  const oStart = start
+  const oEnd = end
+
+  start = date.startOfDate(getMinDate(oStart, oEnd), 'day')
+  end = date.startOfDate(getMaxDate(oStart, oEnd), 'day')
+  const daysBetween = date.getDateDiff(end, start, 'days')
+
+  const dates: Date[] = []
+  for (let daysToAdd = 0; daysToAdd <= daysBetween; daysToAdd++) {
+    dates.push(date.addToDate(start, { days: daysToAdd }))
+  }
+
+  return dates
 }
