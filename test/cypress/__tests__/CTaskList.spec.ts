926f346fd38d44338fc043bfdf6eed9d5a713e3a
Felix <jonfelixrico@gmail.com>
2022-03-13 18:41:41 +0800

    Make CTaskList use the mock task repo

--- test/cypress/__tests__/CTaskList.spec.ts
+++ test/cypress/__tests__/CTaskList.spec.ts
@@ -1,41 +1,15 @@
 import { mount } from '@cypress/vue'
-import { DateTime } from 'luxon'
 import CTaskList from 'src/components/tasks/CTaskList.vue'
-import { TaskRepoKey, TaskRepo } from 'src/services/abstracts/task-repo.service'
+import { TaskRepoKey } from 'src/services/abstracts/task-repo.service'
 import { createI18n } from 'vue-i18n'
-import { ref } from 'vue'
-import { Task } from 'src/typings/task.interface'
-import { range } from 'lodash'
+import {
+  mockDt,
+  mockTaskRepo,
+} from 'app/test/cypress/__tests__/task-repo-mocks'
 
-const snapshotDt = DateTime.fromISO('2022-01-01')
-
-const taskTemplate: Omit<Task, 'id' | 'title'> = {
-  carryOverUntil: snapshotDt,
-  dueDt: snapshotDt,
-  createDt: snapshotDt,
-  lastUpdateDt: snapshotDt,
-  priority: 0,
-  notes: null,
-  completeDt: null,
-}
-
-const mockData: Task[] = range(0, 10).map((rangeNo) => {
-  return {
-    ...taskTemplate,
-    id: String(rangeNo),
-    title: `mock item #${rangeNo}`,
-  }
-})
+const snapshotDt = mockDt
 
 beforeEach(() => {
-  const mockRepo: TaskRepo = {
-    // This is the only method that matters in this unit test
-    getTasks: cy.stub().resolves(mockData),
-    insert: cy.stub(),
-    lastWrite: ref(DateTime.now()),
-    remove: cy.stub(),
-  }
-
   mount(CTaskList, {
     props: {
       snapshotDt,
@@ -45,7 +19,7 @@ beforeEach(() => {
       plugins: [createI18n()],
       provide: {
         // originally of type InjectionKey
-        [TaskRepoKey as symbol]: mockRepo,
+        [TaskRepoKey as symbol]: mockTaskRepo,
       },
     },
   })
