d49b6ca9c5996d0af32759b5936bc1e4a55dd5ed
Felix <jonfelixrico@gmail.com>
2022-03-17 02:25:30 +0800

    Support i18n via labels

--- src/components/tasks/task-form/CTaskForm.vue
+++ src/components/tasks/task-form/CTaskForm.vue
@@ -1,10 +1,9 @@
 <template>
   <!-- Title -->
   <div class="q-gutter-y-sm">
-    <!-- TODO do proper a11y support for labels -->
-    <div class="text-weight-bold">
+    <label for="title" class="text-weight-bold">
       {{ t('task.form.inputs.title.label') }}
-    </div>
+    </label>
 
     <q-input
       v-model="title"
@@ -14,15 +13,15 @@
       dense
       :hint="t('task.form.inputs.title.hint')"
       :rules="[(val) => !!val || t('task.form.inputs.title.error')]"
+      id="title"
     />
   </div>
 
   <!-- Notes -->
   <div class="q-gutter-y-sm">
-    <!-- TODO do proper a11y support for labels -->
-    <div class="text-weight-bold">
+    <label for="notes" class="text-weight-bold">
       {{ t('task.form.inputs.notes.label') }}
-    </div>
+    </label>
 
     <q-input
       v-model="notes"
@@ -32,15 +31,15 @@
       dense
       class="notes-input"
       :hint="t('task.form.inputs.notes.hint')"
+      id="notes"
     />
   </div>
 
   <!-- Priority -->
   <div class="q-gutter-y-sm">
-    <!-- TODO do proper a11y support for labels -->
-    <div class="text-weight-bold">
+    <label for="priority" class="text-weight-bold">
       {{ t('task.form.inputs.priority.label') }}
-    </div>
+    </label>
 
     <q-input
       v-model.number="priority"
@@ -52,15 +51,15 @@
       dense
       :hint="t('task.form.inputs.priority.hint')"
       class="col"
+      id="priority"
     />
   </div>
 
   <!-- Carry-over -->
   <div class="q-gutter-y-sm">
-    <!-- TODO do proper a11y support for labels -->
-    <div class="text-weight-bold">
+    <label for="carryOverDays" class="text-weight-bold">
       {{ t('task.form.inputs.carryOverDays.label') }}
-    </div>
+    </label>
 
     <q-input
       v-model.number="carryOverDays"
@@ -72,6 +71,7 @@
       dense
       :hint="t('task.form.inputs.carryOverDays.hint')"
       class="col"
+      id="carryOverDays"
     />
   </div>
 </template>
