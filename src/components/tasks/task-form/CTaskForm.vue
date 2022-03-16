07f6ecfcd2cdce97b253b0da89591194adfc21d5
Felix <jonfelixrico@gmail.com>
2022-03-17 02:18:09 +0800

    Retrofit CTaskForm

--- src/components/tasks/task-form/CTaskForm.vue
+++ src/components/tasks/task-form/CTaskForm.vue
@@ -1,188 +1,142 @@
 <template>
-  <q-form @submit="$emit('submit', task)" autofocus ref="formRef">
-    <!-- Title -->
-    <div class="q-gutter-y-sm">
-      <!-- TODO do proper a11y support for labels -->
-      <div class="text-weight-bold">
-        {{ t('task.form.inputs.title.label') }}
-      </div>
-      <q-input
-        v-model="task.title"
-        type="textarea"
-        outlined
-        autogrow
-        dense
-        :hint="t('task.form.inputs.title.hint')"
-        :rules="[(val) => !!val || t('task.form.inputs.title.error')]"
-      />
+  <!-- Title -->
+  <div class="q-gutter-y-sm">
+    <!-- TODO do proper a11y support for labels -->
+    <div class="text-weight-bold">
+      {{ t('task.form.inputs.title.label') }}
     </div>
+    <q-input
+      v-model="title"
+      type="textarea"
+      outlined
+      autogrow
+      dense
+      :hint="t('task.form.inputs.title.hint')"
+      :rules="[(val) => !!val || t('task.form.inputs.title.error')]"
+    />
+  </div>
 
-    <!-- Notes -->
-    <div class="q-gutter-y-sm">
-      <!-- TODO do proper a11y support for labels -->
-      <div class="text-weight-bold">
-        {{ t('task.form.inputs.notes.label') }}
-      </div>
-      <q-input
-        v-model="task.notes"
-        type="textarea"
-        outlined
-        autogrow
-        dense
-        class="notes-input"
-        :hint="t('task.form.inputs.notes.hint')"
-      />
+  <!-- Notes -->
+  <div class="q-gutter-y-sm">
+    <!-- TODO do proper a11y support for labels -->
+    <div class="text-weight-bold">
+      {{ t('task.form.inputs.notes.label') }}
     </div>
+    <q-input
+      v-model="notes"
+      type="textarea"
+      outlined
+      autogrow
+      dense
+      class="notes-input"
+      :hint="t('task.form.inputs.notes.hint')"
+    />
+  </div>
 
-    <!-- Priority -->
-    <div class="q-gutter-y-sm">
-      <!-- TODO do proper a11y support for labels -->
-      <div class="text-weight-bold">
-        {{ t('task.form.inputs.priority.label') }}
-      </div>
-      <div class="row items-start q-gutter-x-sm">
-        <q-checkbox
-          :label="t('task.form.inputs.hasPriority.label')"
-          v-model="priority.checkboxModel"
-        />
-        <q-input
-          :disable="!priority.checkboxModel"
-          v-model.number="priority.inputModel"
-          type="number"
-          outlined
-          min="1"
-          max="10"
-          step="0.1"
-          dense
-          :hint="t('task.form.inputs.priority.hint')"
-          class="col"
-        />
-      </div>
-    </div>
+  <!-- Priority -->
+  <div class="q-gutter-y-sm">
+    <!-- TODO add label and use proper layout -->
+    <q-input
+      v-model.number="priority"
+      type="number"
+      outlined
+      min="1"
+      max="10"
+      step="0.1"
+      dense
+      :hint="t('task.form.inputs.priority.hint')"
+      class="col"
+    />
+  </div>
 
-    <!-- Carry-over -->
-    <div class="q-gutter-y-sm">
-      <!-- TODO do proper a11y support for labels -->
-      <div class="text-weight-bold">
-        {{ t('task.form.inputs.carryOver.label') }}
-      </div>
-      <div class="column">
-        <!-- No carry over -->
-        <q-radio
-          v-model="carryOver.radioModel"
-          val="NO_CARRY_OVER"
-          :label="t('task.form.inputs.carryOver.options.no')"
-        />
-
-        <!-- Carry over until date (if not yet completed) -->
-        <q-radio v-model="carryOver.radioModel" val="DEFINITE">
-          <!--
-              The use of `preformatted` here is requried for HTML to honor the space between
-              "...over until {date}"
-            -->
-          <i18n-t
-            keypath="task.form.inputs.carryOver.options.untilDate"
-            tag="div"
-            class="row items-center preformatted"
-            scope="global"
-          >
-            <template #date>
-              <CDateInput
-                outlined
-                dense
-                v-model="carryOver.dateModel"
-                :disable="carryOver.radioModel !== 'DEFINITE'"
-                :bottom-slots="false"
-                hide-clear
-              />
-            </template>
-          </i18n-t>
-        </q-radio>
-      </div>
-    </div>
-  </q-form>
+  <!-- Carry-over -->
+  <div class="q-gutter-y-sm">
+    <!-- TODO add label and use proper layout -->
+    <q-input
+      v-model.number="carryOverDays"
+      type="number"
+      outlined
+      min="0"
+      max="31"
+      step="1"
+      dense
+      :hint="t('task.form.inputs.carryOverDays.hint')"
+      class="col"
+    />
+  </div>
 </template>
 
 <script lang="ts">
 import { useI18n } from 'vue-i18n'
-import { computed, defineComponent, reactive, toRef, PropType, ref } from 'vue'
-import { DraftTaskData } from 'src/typings/task.interface'
-import { useCarryOverInputHelper } from 'components/dialogs/carry-over-input-helper'
-import { usePriorityInputHelper } from 'components/dialogs/priority-input-helper'
-import CDateInput from 'components/common/CDateInput.vue'
+import { computed, defineComponent, PropType } from 'vue'
 import { DateTime } from 'luxon'
-import { QForm } from 'quasar'
 
 interface TaskDraftModel {
-  title: string | null
-  notes: string | null
-  priority: number
-  carryOverUntil: DateTime
-}
-
-export interface TaskDraft extends TaskDraftModel {
-  title: string
-  priority: number
-}
-
-export type TaskCreateInitialData = Omit<DraftTaskData, 'dueDt'>
-function createTaskData(dueDt: DateTime, initialData?: TaskCreateInitialData) {
-  return reactive<TaskDraftModel>({
-    title: initialData?.title ?? null,
-    notes: initialData?.notes ?? null,
-    priority: initialData?.priority ?? 0,
-    carryOverUntil: initialData?.carryOverUntil
-      ? initialData.carryOverUntil
-      : dueDt,
-  })
+  title?: string
+  notes?: string
+  priority?: number
+  carryOverDays?: number
 }
 
 export default defineComponent({
-  components: {
-    CDateInput,
-  },
-
   props: {
     dueDt: {
       type: DateTime,
       required: true,
     },
 
-    initialData: Object as PropType<TaskCreateInitialData>,
+    modelValue: Object as PropType<TaskDraftModel>,
   },
 
-  emits: ['submit'],
+  emits: ['updated:modelValue'],
 
-  setup(props) {
+  setup(props, { emit }) {
     const { t } = useI18n()
 
-    const task = createTaskData(props.dueDt, props.initialData)
-
-    const formattedDueDt = computed(() =>
-      props.dueDt.toLocaleString(DateTime.DATE_MED)
-    )
-
-    const formRef = ref<QForm | null>(null)
-
-    function submit() {
-      if (!formRef.value) {
-        return
-      }
-
-      formRef.value.submit()
+    function emitValue<K extends keyof TaskDraftModel>(
+      key: K,
+      value: TaskDraftModel[K]
+    ) {
+      emit('updated:modelValue', {
+        ...props.modelValue,
+        [key]: value,
+      })
     }
+
+    const title = computed({
+      get: () => props.modelValue?.title,
+      set: (title) => {
+        emitValue('title', title)
+      },
+    })
+
+    const notes = computed({
+      get: () => props.modelValue?.notes,
+      set: (notes) => {
+        emitValue('notes', notes)
+      },
+    })
+
+    const priority = computed({
+      get: () => props.modelValue?.priority ?? 0,
+      set: (priority) => {
+        emitValue('priority', priority)
+      },
+    })
+
+    const carryOverDays = computed({
+      get: () => props.modelValue?.carryOverDays ?? 0,
+      set: (carryOverDays) => {
+        emitValue('carryOverDays', carryOverDays)
+      },
+    })
 
     return {
       t,
-      task,
-      formattedDueDt,
-      carryOver: useCarryOverInputHelper(
-        toRef(task, 'carryOverUntil'),
-        toRef(props, 'dueDt')
-      ),
-      priority: usePriorityInputHelper(toRef(task, 'priority')),
-      submit,
-      formRef,
+      title,
+      notes,
+      priority,
+      carryOverDays,
     }
   },
 })
