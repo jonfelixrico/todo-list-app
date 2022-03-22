3c8469df2ae394215f00f28cecb1ae2f6f52dd0d
Felix <jonfelixrico@gmail.com>
2022-03-23 00:21:13 +0800

    Create V2 of the create dialog

---
+++ src/components/dialogs/CTaskCreateDialogV2.vue
@@ -0,0 +1,43 @@
+<template>
+  <q-dialog ref="dialogRef" @hide="onDialogHide" persistent>
+    <CTaskCreateDialogContent
+      @discard="onDialogCancel"
+      @submit="onDialogOK"
+      :dueDt="dueDt"
+    />
+  </q-dialog>
+</template>
+
+<script lang="ts">
+import { useDialogPluginComponent } from 'quasar'
+import { defineComponent } from 'vue'
+import { DateTime } from 'luxon'
+import CTaskCreateDialogContent from 'src/components/dialogs/CTaskCreateDialogContent.vue'
+
+export default defineComponent({
+  emits: [...useDialogPluginComponent.emits],
+
+  components: {
+    CTaskCreateDialogContent,
+  },
+
+  props: {
+    dueDt: {
+      type: DateTime,
+      required: true,
+    },
+  },
+
+  setup() {
+    const { dialogRef, onDialogHide, onDialogOK, onDialogCancel } =
+      useDialogPluginComponent()
+
+    return {
+      dialogRef,
+      onDialogHide,
+      onDialogOK,
+      onDialogCancel,
+    }
+  },
+})
+</script>
